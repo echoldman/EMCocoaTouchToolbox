9eceab73fe397ad907dfc88afa09775f7447655e
Dong Bin <echoldman@gmail.com>
2014-04-28 16:00:30 +0800

    Fix bug, call 'UIView:y' and processed by 'UIView:x'.

--- EMCocoaTouchToolbox/UIViewEx/UIView+Origin.h
+++ EMCocoaTouchToolbox/UIViewEx/UIView+Origin.h
@@ -10,7 +10,7 @@
 @interface UIView (Origin)
 
 @property (nonatomic, assign, getter = x, setter = setX:) CGFloat x;
-@property (nonatomic, assign, getter = x, setter = setY:) CGFloat y;
+@property (nonatomic, assign, getter = y, setter = setY:) CGFloat y;
 @property (nonatomic, assign, getter = origin, setter = setOrigin:) CGPoint origin;
 @property (nonatomic, assign, readonly, getter = rightX) CGFloat rightX;
 @property (nonatomic, assign, readonly, getter = bottomY) CGFloat bottomY;
